863b688d29d3f9b6e532b632fd9573d427bb6ee0
Ties Stapper <ties.stapper@student.rocva.nl>
2022-02-07 13:31:01 +0100

    1.2

---
+++ 1-Arrays/Opdracht-1.2/index.php
@@ -0,0 +1,12 @@
+<?php
+$persoon["Voornaam"] = "Ties";
+
+$persoon["Achternaam"] = "Stapper";
+
+$persoon["Leeftijd"] = "16";
+
+$persoon["Opleiding"] = "Software Development";
+
+echo $persoon['Voornaam']. " " . $persoon['Achternaam']. " is " .  $persoon['Leeftijd']. " jaar oud" .  " en is student van het ROCvA en volgt de opleiding " . $persoon['Opleiding'] ;
+
+?>
