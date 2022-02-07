ac8f6eed69350c54e07c9441dbc39bc09173befc
Ties Stapper <ties.stapper@student.rocva.nl>
2022-02-07 23:35:16 +0100

    2.2

---
+++ 2-Loops/Opdracht-2.2/index.php
@@ -0,0 +1,12 @@
+<?php
+
+echo "<ul>";
+
+for($leeftijd = 0; $leeftijd < 18; $leeftijd++){
+    echo"<li>Ik ben " . $leeftijd . "jaar, dus ik mag nog niet stemmen.</li>";
+}
+
+echo "<li>Ik ben " . $leeftijd . "jaar oud, dus ik heb stemrecht!</li>";
+
+echo "</ul>";
+?>
