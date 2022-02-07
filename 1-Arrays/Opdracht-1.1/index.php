0f54bd16904cd5e1c4c38428bed5c9232506d306
Ties Stapper <ties.stapper@student.rocva.nl>
2022-02-07 12:56:56 +0100

    1.1

---
+++ 1-Arrays/Opdracht-1.1/index.php
@@ -0,0 +1,18 @@
+<?php
+$student = array ("voornaam"=>"Ties","achternaam"=>"Stapper","Klas"=>"1C","leeftijd"=>"16");
+echo "Gegevens van Chahid Laaouar:";
+echo "<br>";
+
+echo "Voornaam: " . $student["voornaam"];
+echo "<br>";
+
+echo "Achternaam: " . $student["achternaam"];
+echo "<br>";
+
+echo "Klas: " . $student["Klas"];
+echo "<br>";
+
+echo "Leeftijd: " . $student["leeftijd"] ."jaar";
+echo "<br>";
+
+?>
