492f477229e58ed2c40f00f9340d23d9b1596dd6
Ties Stapper <ties.stapper@student.rocva.nl>
2022-02-11 13:10:10 +0100

    3.1

---
+++ 3-Forms/Opdracht-3.1/mijngegevens.php
@@ -0,0 +1,14 @@
+<?php
+echo 'Naam: ';
+echo $_POST["voornaam"];
+echo ' ';
+echo $_POST["Achternaam"];
+echo '<br> Klas: ';
+echo $_POST["Klas"];
+echo '<br> Leeftijd: ';
+echo $_POST["Leeftijd"];
+echo '<br> Adres: ';
+echo $_POST["Adres"];
+echo '<br> Woonplaats: ';
+echo $_POST["Woonplaats"];
+?> 
