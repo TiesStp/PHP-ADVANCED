8df17f8e63cf86a2359578ac938a0f301b2599a6
Ties Stapper <ties.stapper@student.rocva.nl>
2022-02-15 10:07:07 +0100

    3.3

---
+++ 3-Forms/Opdracht-3.3/mijnmuziek.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h3>Lievelings nummer:</h3>
+<form name id="muziek" action = "mijngegevens.php" method="get">
+<button>
+<a href="mijngegevens.php?
+artiest=Sleepy Hollow&
+titel=Basketball dreams&
+JaarVanUitkomst=2019&
+muzieksoort=Engelse rap">Meer informatie</a>
+</button>
+</form>
+</body>
+</html>
