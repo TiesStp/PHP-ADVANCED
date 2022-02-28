d1ebc052730f0b74cdfe1999f4b8a84f4e838bc7
Ties Stapper <ties.stapper@student.rocva.nl>
2022-02-28 22:38:24 +0100

    3.2

--- 3-Forms/Opdracht-3.2/mijngegevens.php
+++ 3-Forms/Opdracht-3.2/mijngegevens.php
@@ -1,13 +1,18 @@
-<!DOCTYPE HTML>
-<html>  
-<body>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<?php 
+echo "<body style= \"background-color:".$_POST['color'].";\" >";
+?>
 Voornaam is: <?php  echo $_POST["Voornaam"]; ?> <br>
 Achternaam :<?php echo $_POST["achternaam"];?><br>
 Klas: <?php echo $_POST["klas"];?> <br>
 leeftijd: <?php echo $_POST["leeftijd"];?> <br>
 adres: <?php echo$_POST["adres"];  ?> <br>
 plaatsnaam :<?php echo $_POST["plaatsnaam"];?> <br>
-favoriete muziekband: <?php echo $_POST["muziek_band"];?> <br>
+<?php echo$_POST["geslacht"];?> <br>
+<?php echo $_POST["color"];?>
 
 </body>
 </html> 
